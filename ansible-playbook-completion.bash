42d5eb4faa5813a5e85dc7d3392c6e6773d0296d
irgeek-sb <James.Sinclair@sportsbet.com.au>
2016-08-25 10:27:09 +1000

    Add missing options to ansible-playbook completion.

--- ansible-playbook-completion.bash
+++ ansible-playbook-completion.bash
@@ -2,17 +2,19 @@
 
 _ansible-playbook() {
     local current_word=${COMP_WORDS[COMP_CWORD]}
-    local options="--ask-become-pass -k --ask-pass --ask-su-pass
-                   -K --ask-sudo-pass --ask-vault-pass -b --become
-                   --become-method --become-user -C --check -c
-                   --connection -D --diff -e --extra-vars --flush-cache
-                   --force-handlers -f --forks -h --help -i
-                   --inventory-file -l --limit --list-hosts
-                   --list-tags --list-tasks -M --module-path
-                   --private-key --skip-tags --start-at-task
-                   --step -S --su -R --su-user -s --sudo -U --sudo-user
-                   --syntax-check -t --tags -T --timeout -u --user
-                   --vault-password-file -v --verbose --version"
+    local options="--ask-become-pass --ask-pass --ask-su-pass
+                   --ask-sudo-pass --ask-vault-pass --become
+                   --become-method --become-user --check --connection
+                   --diff --extra-vars --flush-cache --force-handlers
+                   --forks --help --inventory-file --key-file
+                   --limit --list-hosts --list-tags --list-tasks
+                   --module-path --new-vault-password-file --output
+                   --private-key --scp-extra-args --sftp-extra-args
+                   --skip-tags --ssh-common-args --ssh-extra-args
+                   --start-at-task --step --su --su-user --sudo
+                   --sudo-user --syntax-check --tags --timeout --user
+                   --vault-password-file --verbose --version -b -c -e
+                   -f -h -i -k -l -s -t -u -v"
 
     if [[ "$current_word" == -* ]]; then
         COMPREPLY=( $( compgen -W "$options" -- "$current_word" ) )
